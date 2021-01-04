660fbbabcb0fd7b5251467a506d095239349288a
ychu0213@gmail.com <ychu0213@gmail.com>
2021-01-04 13:54:54 +0900

    add FT

--- timecorrelation.py
+++ timecorrelation.py
@@ -18,19 +18,20 @@ import numpy as np
 from dump import readdump
 import pandas as pd 
 
-def Orderlife(dumpfile, orderings, ndim = 3, dt = 0.002, outputfile = ''):
+
+def Orderlife(dumpfile, orderings, ndim=3, dt=0.002, outputfile='', filetype='lammps', moltypes=''):
     """Calculate the time decay of structural ordering
 
-    orders is the numpy array of structural ordering
+    orderings is the numpy array of structural ordering
     its shape is [atom_number, snapshot_number]
     this is the standard output format of this analysis package:
-    READ: orders = np.loadtxt(orderfile, skiprows = 1)[:, 1:]
+    READ: orderings = np.loadtxt(orderfile, skiprows = 1)[:, 1:]
 
     dumpfile is used to get the time scale
     """
 
     print ('------------Calculate Order time self-correlation---------')
-    d = readdump(dumpfile, ndim)
+    d = readdump(dumpfile, ndim, filetype, moltypes)
     d.read_onefile()
 
     TimeStep = d.TimeStep[1] - d.TimeStep[0]
@@ -61,25 +62,26 @@ def Orderlife(dumpfile, orderings, ndim = 3, dt = 0.002, outputfile = ''):
     print ('------------Calculate Order time self-correlation Over---------')
     return results, names
 
-def logOrderlife(dumpfile, orderings, ndim = 3, dt = 0.002, outputfile = ''):
+
+def logOrderlife(dumpfile, orderings, ndim=3, dt=0.002, outputfile='', filetype='lammps', moltypes=''):
     """Calculate the time decay of structural ordering with 
     the initial configuration as reference
 
-    orders is the numpy array of structural ordering
+    orderings is the numpy array of structural ordering
     its shape is [atom_number, snapshot_number]
     this is the standard output format of this analysis package:
-    READ: orders = np.loadtxt(orderfile, skiprows = 1)[:, 1:]
+    READ: orderings = np.loadtxt(orderfile, skiprows = 1)[:, 1:]
 
     dumpfile is used to get the time scale
     """
 
     print ('------------Calculate LOG Order time self-correlation---------')
-    d = readdump(dumpfile, ndim)
+    d = readdump(dumpfile, ndim, filetype, moltypes)
     d.read_onefile()
 
     if d.SnapshotNumber != orderings.shape[1]:
         errorinfo = '***inconsistent number of configurations and orderings***'
-        raise ValueError
+        raise ValueError(errorinfo)
 
     results = np.zeros((d.SnapshotNumber - 1, 2))
     names = 't  St'
@@ -96,3 +98,58 @@ def logOrderlife(dumpfile, orderings, ndim = 3, dt = 0.002, outputfile = ''):
 
     print ('------------Calculate LOG Order time self-correlation Over---------')
     return results, names
+
+
+def Orderlife_FT(dumpfile, orderings, qvector, ndim=3, dt=0.002, outputfile='', filetype='lammps', moltypes=''):
+    """calculate the time decay of structural ordering in reciprocal space
+        by Fourier Transformation
+
+        orderings is the numpy array of structural ordering
+        its shape is [atom_number, snapshot_number]
+        this is the standard output format of this analysis package:
+        READ: orderings = np.loadtxt(orderfile, skiprows=1)[:, 1:]
+
+        dumpfile is used to get the timescale and the FFT
+
+        qvector is the designed wavevector, such as [1, 0, 0] for 3D
+    """
+
+    print ("------Calculate Order time self-correlation by FT------")
+    d = readdump(dumpfile, ndim, filetype, moltypes)
+    d.read_onefile()
+
+    print (d.SnapshotNumber)
+    print (orderings.shape)
+
+    TimeStep = d.TimeStep[1] - d.TimeStep[0]
+    if TimeStep != d.TimeStep[-1] - d.TimeStep[-2]:
+        print('Warning: ********time interval changes************')
+
+    twopidl = 2.0*np.pi/d.Boxlength[0]
+    qvector *= twopidl
+    qvalue = np.linalg.norm(qvector)
+
+    #calculate FT for each configuration
+    sinparts = np.zeros(d.SnapshotNumber)
+    cosparts = np.zeros_like(sinparts)
+    for n in range(d.SnapshotNumber):
+        theta = (qvector * d.Positions[n]).sum(axis=1)
+        sinparts[n] = np.sum(orderings[:, n]*np.sin(theta))/orderings[:, n].sum()
+        cosparts[n] = np.sum(orderings[:, n]*np.cos(theta))/orderings[:, n].sum()
+    
+    #calculate time correlation
+    results = np.zeros((d.SnapshotNumber, 2))
+    counts = np.zeros(d.SnapshotNumber)
+    for n in range(d.SnapshotNumber):
+        for nn in range(n+1):
+            results[nn, 1] += sinparts[n]*sinparts[n-nn]+cosparts[n]*cosparts[n-nn]
+            counts[nn] += 1
+    
+    results[:, 1] /= counts
+    results[:, 0] = (np.array(d.TimeStep)-d.TimeStep[0])*dt
+    
+    if outputfile:
+        np.savetxt(outputfile, results, fmt='%.6f', header='t Sqt', comments='qvalue=%.3f; length=%.3f\n'%(qvalue, 2.0*np.pi/qvalue))
+    
+    print("------Calculate Order time self-correlation by FFT Over------")
+    return results, qvalue
